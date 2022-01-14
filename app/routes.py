1b02d7e9b26d873fd31e5ce1c02fc76b4fb7d6b7
Jeff <jefnic23@gmail.com>
2022-01-14 01:27:36 -0500

    added admin stuff

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
 from flask import render_template, redirect, url_for, flash
+from flask_login import LoginManager, login_user, current_user, logout_user
 from flask_bootstrap import Bootstrap
-from forms import ContactForm
+from forms import *
+from models import *
 import glob, os
+from app.email import send_password_reset_email
 from app import app
 
 bootstrap = Bootstrap(app)
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route('/')
 def home():
@@ -31,5 +40,63 @@ def contact():
         return redirect(url_for('contact'))
     return render_template('contact.html', form=contact_form)
 
+@app.route('/admin')
+def admin():
+    if current_user.is_anonymous:
+        return redirect(url_for('login'))
+    return render_template('admin.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user_object = User.query.filter_by(email=login_form.email.data).first()
+        if not user_object or not user_object.check_password(login_form.password.data):
+            flash('Invalid username or password', 'danger')
+            return redirect(url_for('login'))
+        login_user(user_object, remember=login_form.remember_me.data)
+        flash('Login successful', 'success')
+        return redirect(url_for('admin'))
+    return render_template("login.html", form=login_form)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    if current_user.is_anonymous:
+        return redirect(url_for("index"))
+    logout_user()
+    flash("You have logged out successfully", "success")
+    return redirect(url_for("login"))
+
+@app.route("/reset_password_request", methods=['GET', 'POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        db.session.commit()
+        if user:
+            send_password_reset_email(user)
+        flash("Check your email for instructions on how to reset your password", 'info')
+        return redirect(url_for('login'))
+    return render_template("reset_password_request.html", form=form)
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        password = form.password.data
+        hashed_pswd = pbkdf2_sha256.hash(password)
+        user.set_password(hashed_pswd)
+        db.session.commit()
+        flash('Your password has been reset', "success")
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
