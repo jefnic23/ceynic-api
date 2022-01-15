0f42c944ee2422dd50efb974276b3d505f92402b
Jeff <jefnic23@gmail.com>
2022-01-15 00:38:08 -0500

    config fix

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgresql://mzbayjsjwpxhmf:fb81fdceabeaa7e6703d0922ed241c8040777b9446415a72766aa0b1134e7ca9@ec2-52-44-58-234.compute-1.amazonaws.com:5432/d1klm9o9rdhe86"
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
