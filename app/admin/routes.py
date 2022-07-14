c0b21e2db738d3ea7a9714091661a25dac2179fe
Jeff <jefnic23@gmail.com>
2022-07-14 15:01:17 -0400

    drag and reorder dropzone

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, jsonify, request, current_app
+from flask import render_template, redirect, url_for, flash, request, current_app
 from flask_login import login_required
 from sqlalchemy import func
 from app import db
@@ -24,20 +24,33 @@ def product():
 @login_required
 def create():
     if request.method == 'POST':
-        print(f'\n{request.files}\n')
+        title = request.form['title']
+        price = request.form['price']
+        medium = request.form['medium']
+        height = request.form['height']
+        width = request.form['width']
+        description = request.form['description']
+        slideshow = request.form.get('slideshow')
         flash("Product added.", "success")
-        return '', 204
+        return redirect(url_for('admin.product'))
     else:
         return render_template('admin/create.html')
     
 
-@bp.route('/update/<int:id>')
+@bp.route('/update/<int:id>', methods=['GET', 'POST'])
 @login_required
 def update(id):
-    return render_template('admin/update.html')
+    if request.method == 'POST':
+        print(request.form['title'])
+        return '', 204
+    else:
+        listing = Product.query.get(id)
+        files = [f'{current_app.config["AWS_URL"]}{f.key}' for f in current_app.bucket.objects.filter(Prefix=f"public/{listing.title.replace(' ', '_')}/")]
+        return render_template('admin/update.html', listing=listing, files=files)
 
 
-@bp.route('/delete')
+@bp.route('/delete/<int:id>')
 @login_required
-def delete():
+def delete(id):
+    print(f"\n{id}\n")
     return render_template('admin/delete.html')
--- app/static/js/form.js
+++ app/static/js/form.js
@@ -46,9 +46,7 @@ function previewFile(file) {
         icon.setAttribute('onclick', 'removeImage(this)');
         img.src = reader.result;
         img.id = file.name;
-        img.draggable = false;
         img.className = "dz-img";
-        img.ondragstart = 'return false';
         div.appendChild(icon);
         div.appendChild(img);
         previews.appendChild(div);
@@ -62,17 +60,12 @@ function handleFiles(files) {
             previewFile(f);
         }
     });
-    let dt = new DataTransfer();
-    img_files.forEach((f) => {
-        dt.items.add(f);
-    });
-    images.files = dt.files;
+    setImages();
 }
 
 dropzone.addEventListener('drop', handleDrop, false);
 
 function handleDrop(e) {
-    console.log(e.dataTransfer.files);
     if (e.dataTransfer.files.length > 0) {
         let files = e.dataTransfer.files;
         handleFiles(files)
@@ -80,6 +73,61 @@ function handleDrop(e) {
 }
 
 function removeImage(e) {
-    console.log(e.parentNode);
-    console.log(e.nextElementSibling.id);
+    img_files = img_files.filter(img => img.name !== e.nextElementSibling.id);
+    e.parentNode.remove();
+    setImages();
 }
+
+function setImages() {
+    let dt = new DataTransfer();
+    img_files.forEach((f) => {
+        dt.items.add(f);
+    });
+    images.files = dt.files;
+}
+
+function prefill(files) {
+    [...files].forEach((f) => {
+        createFile(f).then((res) => {
+            if (!img_files.some(img => img.name === res.name)) {
+                uploadFile(res);
+                previewFile(res);
+                setImages();
+            }
+        });
+    });
+}
+
+async function createFile(e) {
+    let fname = e.split('/').at(-1);
+    let res = await fetch(e);
+    let data = await res.blob();
+    let metadata = {
+        name: fname,
+        type: 'image/*'
+    };
+    return file = new File([data], fname, metadata);
+}
+
+function mapOrder (array, order, key) {
+    array.sort( function (a, b) {
+      var A = a[key], B = b[key];
+      if (order.indexOf(A) > order.indexOf(B)) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    return array;
+};
+
+Sortable.create(previews, {
+    onEnd: function() {
+        let sorted = [];
+        [...previews.children].forEach((e) => {
+            sorted = [...sorted, e.lastChild.id];
+        });
+        img_files = mapOrder(img_files, sorted, 'name');
+        setImages();
+    }
+});
