ad28385703acb9449d68760b75a141bacc8e684e
Jeff <jefnic23@gmail.com>
2022-10-13 18:05:54 -0400

    added sender info to email body

--- app/email.py
+++ app/email.py
@@ -11,6 +11,6 @@ def send_async_email(app, msg):
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
+    msg.body = text_body + sender
+    msg.html = html_body + sender
     Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
