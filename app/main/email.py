f333945cd92218f01495470352ac71ca292330e5
Jeff <jefnic23@gmail.com>
2022-10-16 15:22:57 -0400

    Mailgun configuration

--- app/main/email.py
+++ app/main/email.py
@@ -1,11 +1,12 @@
 from flask import render_template
 from app.email import send_email
+from flask import current_app
 
 
 def send_contact_email(form):
     send_email(f'[TraceyNicholasArt] Message from {form.name.data}',
         sender=(form.name.data, form.email.data),
-        recipients=['info@traceynicholas.com'],
+        recipients=[current_app.config['EMAIL_RECIPIENT']],
         text_body=render_template('email/contact_me.txt', msg=form.msg.data),
         html_body=render_template('email/contact_me.html', msg=form.msg.data))
         
