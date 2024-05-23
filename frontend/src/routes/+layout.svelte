ae4baf32d1ff2a68ac220a82405540b45084fb3a
Jeffrey Nicholas <jefnic23@gmail.com>
2024-05-22 22:33:09 -0400

    ✨ (frontend): add a new layout component for navigation links and styling 📝 (frontend): create a new page component with an image import and styling for header and title

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/browse">Browse art</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</nav>
+
+<slot></slot>
+
+<style>
+    nav {
+        min-height: 50px;
+        background-color: #f8f8f8;
+        border: 1px solid #e7e7e7;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    nav a {
+        text-transform: uppercase;
+        text-decoration: none;
+        margin: 0 0.5rem;
+        color: rgba(0, 0, 0, .55);
+        font-size: 13px;
+        font-family: 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import image from "$lib/header.jpg"
+</script>
+
+<img src={image} alt="header.jpg" />
+<h1>Original Abstract Oil Paintings on Canvas and Prints</h1>
+
+<style>
+    img {
+        max-width: 100%;
+        border-bottom: 1px solid #e7e7e7;
+        position: relative;
+        top: 50px;
+    }
+</style>
