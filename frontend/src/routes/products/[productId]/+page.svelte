840b4aa47a3c6e96f4ec8bea59ae959c678fcb49
Jeffrey Nicholas <jefnic23@gmail.com>
2024-05-25 21:31:45 -0400

    ✨ (frontend): Add new Svelte page component for displaying product details 📝 (frontend): Create a new Svelte page component to fetch and display product information, including title, description, price, images, and shipping details. The component also includes a PayPal component for handling payments.

---
+++ frontend/src/routes/products/[productId]/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import type { PageData } from "./$types";
+    import PayPal from "$lib/PayPal.svelte";
+
+    export let data: PageData;
+</script>
+
+<div class="wrapper">
+    {#await data.product}
+        <p>Loading product...</p>
+    {:then product} 
+        <img src="{product.images[0]}" alt={product.title} />
+        <div class="row">
+            <div class="column" style="flex: 3;">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+            </div>
+            <div class="column" style="flex: 2;">
+                <div>
+                    <div>
+                        ${product.price}
+                    </div>
+                    <div>
+                        + FREE shipping
+                    </div>
+                    <PayPal amount={product.price} />
+                </div>
+                <div>
+                    <div>
+                        ships from
+                    </div>
+                    <div>
+                        Pinckney, MI
+                    </div>
+                    <div>
+                        returns & exchanges
+                    </div>
+                    <div>
+                        You have 14 days from item delivery to ship this item back. Buyer is responsible for return shipping costs. If the item is not returned in its original condition, the buyer is responsible for any loss in value.
+                    </div>
+                </div>
+            </div>
+        </div>
+    {/await}
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+    }
+
+    .row {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .column {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .column * {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
