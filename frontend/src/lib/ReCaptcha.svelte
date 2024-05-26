37d2f161a7bf249b560388faf25b71130ed24226
Jeffrey Nicholas <jefnic23@gmail.com>
2024-05-25 21:31:38 -0400

    ✨ (ReCaptcha.svelte): add ReCaptcha component to handle Google reCAPTCHA integration for form submission in the contact page 📝 (contact/+page.svelte): replace Google reCAPTCHA script and button with ReCaptcha component to simplify and improve code readability and maintainability

---
+++ frontend/src/lib/ReCaptcha.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+	<script src="https://www.google.com/recaptcha/api.js"></script>
+
+	<script>
+		function onSubmit(token) {
+			console.log(token);
+			// document.getElementById('contact-form').submit();
+		}
+	</script>
+</svelte:head>
+
+<button
+    class="g-recaptcha"
+    data-sitekey="6LfZN-gpAAAAAG-Apw86KAsXLC4Yp52XefxzJi1i"
+    data-callback="onSubmit"
+    data-action="submit"
+>
+    Send
+</button>
