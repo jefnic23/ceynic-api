fe47e622edfd814f749c0df61f2bea5a026c6e61
Jeffrey Nicholas <jefnic23@gmail.com>
2024-05-30 22:33:23 -0400

    ♻️ (order.py): remove unused imports and fields to clean up the code and improve readability

--- backend/src/models/order.py
+++ backend/src/models/order.py
@@ -1,10 +1,6 @@
-from datetime import datetime
 from typing import TYPE_CHECKING
-from decimal import Decimal
 
-from sqlmodel import Column, DateTime, Field, Relationship, SQLModel
-
-from backend.src.models.enums.order_status import OrderStatus
+from sqlmodel import Field, Relationship, SQLModel
 
 if TYPE_CHECKING:
     from backend.src.models.product import Product
@@ -14,17 +10,5 @@ class Order(SQLModel, table=True):
     __tablename__ = "orders"
 
     id: str = Field(primary_key=True)
-    status: OrderStatus
-    create_time: datetime = Field(sa_column=Column(DateTime(timezone=True)))
-    address_1: str
-    address_2: str
-    city: str
-    state: str
-    postal_code: str
-    country: str
-    payer_first_name: str
-    payer_last_name: str
-    payer_email: str
-    amount: Decimal
 
     products: list["Product"] = Relationship(back_populates="order")
