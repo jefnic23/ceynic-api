68e6deb226839867d3099b7fc176ffe6fcb9c063
Jeffrey Nicholas <jefnic23@gmail.com>
2024-06-01 13:05:05 -0400

    ✨ (Footer.svelte): Add a new Footer component to display social media icons and copyright information 📝 (layout.svelte): Refactor layout to include the new Footer component and update navigation links to match the design of the website

---
+++ frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+    import Etsy from '$lib/icons/etsy.svelte';
+	import Instagram from '$lib/icons/instagram.svelte';
+	import Pinterest from '$lib/icons/pinterest.svelte';
+
+    let year: number = new Date().getFullYear();
+</script>
+
+<div class="footer">
+    <div class="icons">
+        <a href="https://www.etsy.com/shop/TraceyNicholasArt" target="_blank" class="icon etsy">
+            <Etsy />
+        </a>
+        <a
+            href="https://www.instagram.com/traceynicholas_art/"
+            target="_blank"
+            class="icon instagram"
+        >
+            <Instagram />
+        </a>
+        <a href="https://www.pinterest.com/tnicholas48169/" target="_blank" class="icon pinterest">
+            <Pinterest />
+        </a>
+    </div>
+    <div class="copyright">
+        Copyright © {year}, Tracey Nicholas. All rights reserved. |
+        <a href="/login" style="color: #666666;">Administration</a> | Site by
+        <a href="https://github.com/jefnic23" target="_blank">Jeff Nicholas</a>
+    </div>
+</div>
+
+<style>
+    .footer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+		padding: 1rem;
+		background-color: #f8f8f8;
+		border: 1px solid #e7e7e7;
+	}
+
+	.icons {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	.icon {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		height: 32px;
+		width: 32px;
+	}
+
+	.etsy {
+		border: #fd7e14 solid 1px;
+		border-radius: 50%;
+		background-color: #fd7e14;
+		padding: 3px;
+		color: white;
+	}
+
+	.instagram {
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	.pinterest {
+		color: red;
+	}
+
+	.copyright {
+		font-size: 10px;
+	}
+</style>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -2,30 +2,25 @@
 	import { fly } from 'svelte/transition';
 	import { cubicIn, cubicOut } from 'svelte/easing';
 	import '../app.css';
-	import Etsy from '$lib/icons/etsy.svelte';
-	import Instagram from '$lib/icons/instagram.svelte';
-	import Pinterest from '$lib/icons/pinterest.svelte';
 	import image from '$lib/header.jpg';
 	import type { PageData } from './$types';
 	import Header from '$lib/components/Header.svelte';
-
-	let year: number = new Date().getFullYear();
+	import Footer from '$lib/components/Footer.svelte';
 
 	export let data: PageData;
 </script>
 
 <div class="wrapper">
-	<div>
-		<Header>
-			<nav>
-				<a href="/" data-sveltekit-preload-data>Home</a>
-				<a href="/browse" data-sveltekit-preload-data>Browse</a>
-				<a href="/about">About</a>
-				<a href="/contact" data-sveltekit-preload-data>Contact</a>
-			</nav>
-		</Header>
-		<img class="hero" src={image} alt="header.jpg" />
-	</div>
+	<Header>
+		<nav>
+			<a href="/" data-sveltekit-preload-data>Home</a>
+			<a href="/products" data-sveltekit-preload-data>Browse</a>
+			<a href="/about">About</a>
+			<a href="/contact">Contact</a>
+		</nav>
+	</Header>
+	
+	<img class="hero" src={image} alt="header.jpg" />
 
 	{#key data.url}
 		<main
@@ -36,28 +31,7 @@
 		</main>
 	{/key}
 
-	<div class="footer">
-		<div class="icons">
-			<a href="https://www.etsy.com/shop/TraceyNicholasArt" target="_blank" class="icon etsy">
-				<Etsy />
-			</a>
-			<a
-				href="https://www.instagram.com/traceynicholas_art/"
-				target="_blank"
-				class="icon instagram"
-			>
-				<Instagram />
-			</a>
-			<a href="https://www.pinterest.com/tnicholas48169/" target="_blank" class="icon pinterest">
-				<Pinterest />
-			</a>
-		</div>
-		<div class="copyright">
-			Copyright © {year}, Tracey Nicholas. All rights reserved. |
-			<a href="/login" style="color: #666666;">Administration</a> | Site by
-			<a href="https://github.com/jefnic23" target="_blank">Jeff Nicholas</a>
-		</div>
-	</div>
+	<Footer />
 </div>
 
 <style>
@@ -105,52 +79,5 @@
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
-	}
-
-	.footer {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 1rem;
-		padding: 1rem;
-		background-color: #f8f8f8;
-		border: 1px solid #e7e7e7;
-	}
-
-	.icons {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		gap: 1rem;
-	}
-
-	.icon {
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		align-items: center;
-		height: 32px;
-		width: 32px;
-	}
-
-	.etsy {
-		border: #fd7e14 solid 1px;
-		border-radius: 50%;
-		background-color: #fd7e14;
-		padding: 3px;
-		color: white;
-	}
-
-	.instagram {
-		background-clip: text;
-		-webkit-text-fill-color: transparent;
-	}
-
-	.pinterest {
-		color: red;
-	}
-
-	.copyright {
-		font-size: 10px;
 	}
 </style>
