abf4a8504ef2379c83a21b176524515afdb130bf
Jeffrey Nicholas <jefnic23@gmail.com>
2024-08-04 16:00:51 -0400

    reorganizing for grouped layouts

---
+++ frontend/src/routes/admin/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { fly } from 'svelte/transition';
+	import { cubicIn, cubicOut } from 'svelte/easing';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+</script>
+
+{#key data.url}
+    <main
+        in:fly={{ y: -34, duration: 144, delay: 233, easing: cubicOut }}
+        out:fly={{ y: 34, duration: 144, easing: cubicIn }}
+    >
+        <slot />
+    </main>
+{/key}
---
+++ frontend/src/routes/admin/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Modal from "$lib/components/Modal.svelte";
+
+    let showModal: boolean = true;
+</script>
+
+<Modal bind:showModal>
+    <h2 slot="header">
+        Login
+    </h2>
+    <form>
+        <label for="email">Email</label>
+        <input id="email" name="email" value="" placeholder="Your email..." required />
+        <label for="password">Email</label>
+        <input id="password" name="password" value="" placeholder="Your password..." required />
+        <button>Submit</button>
+    </form>
+</Modal>
