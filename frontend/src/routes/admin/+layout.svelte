90143f17e5ef19dfb6adfe7e75c7afb1e4e33eea
Jeffrey Nicholas <jefnic23@gmail.com>
2024-11-02 22:24:43 -0400

    ✨ (admin layout): introduce new layout server file to manage user state and URL ♻️ (admin layout): refactor layout and page components to use new layout server 📝 (admin layout): remove outdated page server and layout files for cleaner structure 💡 (admin layout): add styles for container and main elements to improve layout

--- frontend/src/routes/admin/+layout.svelte
+++ frontend/src/routes/admin/+layout.svelte
@@ -1,16 +1,49 @@
 <script lang="ts">
-    import { fly } from 'svelte/transition';
+	import type { LayoutData } from './$types';
+	import { fly } from 'svelte/transition';
 	import { cubicIn, cubicOut } from 'svelte/easing';
-    import type { PageData } from './$types';
+	import Login from '$lib/components/Login.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	
 
-    export let data: PageData;
+	export let data: LayoutData;
+
+	let user = data.user;
+	$: showModal = !user;
+
+	function handleLoginSuccess(event: any) {
+		user = event.detail.user;
+	}
 </script>
 
-{#key data.url}
-    <main
-        in:fly={{ y: -34, duration: 144, delay: 233, easing: cubicOut }}
-        out:fly={{ y: 34, duration: 144, easing: cubicIn }}
-    >
-        <slot />
-    </main>
-{/key}
+{#if showModal}
+	<Login on:loginSuccess={handleLoginSuccess} />
+{:else}
+	<div class="container">
+		<Sidebar />
+    
+		{#key data.url}
+			<main
+				in:fly={{ y: -34, duration: 144, delay: 233, easing: cubicOut }}
+				out:fly={{ y: 34, duration: 144, easing: cubicIn }}
+			>
+				<slot />
+			</main>
+		{/key}
+	</div>
+{/if}
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: row;
+	}
+
+	main {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: flex-start;
+		margin: 1rem;
+	}
+</style>
--- frontend/src/routes/admin/+page.svelte
+++ frontend/src/routes/admin/+page.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-	import Login from '$lib/components/Login.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-
-	let user = data.user;
-	$: showModal = !user;
-
-	function handleLoginSuccess(event: any) {
-		user = event.detail.user;
-	}
 </script>
 
-{#if showModal}
-	<Login on:loginSuccess={handleLoginSuccess} />
-{:else}
+<div class="wrapper">
 	<div>
-		<h1>hello world</h1>
+		some content
 	</div>
-{/if}
+</div>
