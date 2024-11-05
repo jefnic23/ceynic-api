639fbba12e96d57b1eb42cb1a93e3612ad449020
Jeffrey Nicholas <jefnic23@gmail.com>
2024-11-04 22:08:07 -0500

    ✨ (product.ts): add thumbnail property to Product interface for better representation of product data ✨ (product.ts): create ProductsOut and ProductOut interfaces to extend Product with additional properties for API responses ♻️ (page.ts): refactor fetchProducts function to return ProductsOut instead of Product for improved type safety and consistency with API response structure

--- frontend/src/lib/interfaces/product.ts
+++ frontend/src/lib/interfaces/product.ts
@@ -1,9 +1,18 @@
-export interface Product {
+interface Product {
     id: number;
     title: string;
     price: number;
     height: number;
     width: number;
     medium: "Painting" | "Print";
+    thumbnail: string;
+}
+
+export interface ProductsOut extends Product {
     imageUrl: string;
+}
+
+export interface ProductOut extends Product {
+    description: string | null;
+    images: string[];
 }
--- frontend/src/routes/admin/products/+page.ts
+++ frontend/src/routes/admin/products/+page.ts
@@ -1,7 +1,7 @@
-import type { Product } from '$lib/interfaces/product';
+import type { ProductsOut } from '$lib/interfaces/product';
 
 export const load = async ({ fetch }) => {
-    const fetchProducts = async (): Promise<Product[]> => {
+    const fetchProducts = async (): Promise<ProductsOut[]> => {
         const response = await fetch('http://127.0.0.1:8000/products');
 
         if (!response.ok) {
@@ -12,13 +12,14 @@ export const load = async ({ fetch }) => {
         const responseData = await response.json();
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return responseData.map((item: any): Product => ({
+        return responseData.map((item: any): ProductsOut => ({
             id: item.id,
             title: item.title,
             price: item.price,
             height: item.height,
             width: item.width, 
             medium: item.mediumId === 1 ? "Painting" : "Print",
+            thumbnail: item.thumbnail,
             imageUrl: item.imageUrl
         }));
     }
