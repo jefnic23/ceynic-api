6b06f4f36c18c65b21ab2c4914ac3d0bacf2f51b
Jeffrey Nicholas <jefnic23@gmail.com>
2024-11-05 21:18:12 -0500

    ✨ (Info.svelte): add new Info icon component to enhance UI with visual cues ✨ (products/+page.svelte): integrate Info icon in the product edit form header for better user experience ♻️ (products/+page.svelte): refactor form layout for improved readability and organization of input fields

--- frontend/src/routes/admin/products/+page.svelte
+++ frontend/src/routes/admin/products/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Modal from '$lib/components/Modal.svelte';
+	import Info from '$lib/icons/Info.svelte';
 	import Thumbnail from '$lib/icons/Thumbnail.svelte';
 	import type { ProductOut, ProductsOut } from '$lib/interfaces/product';
 	import type { PageData } from './$types';
@@ -95,35 +96,58 @@
 			<div>loading product...</div>
 		{:else}
 			<div class="edit">
-				<label for="title">Title</label>
-				<input name="title" type="text" value={selectedProduct.title} />
+				<div class="edit-header">
+					<Info color="#1e90ff" width="2em" height="2em" />
+					<span>Edit Product</span>
+				</div>
 
-				<label for="price">Price</label>
-				<div class="price">
-					<input name="price" type="number" min="0.01" step="0.01" value={selectedProduct.price} />
+				<div class="form-input">
+					<label for="title">Title</label>
+					<input id="title" type="text" value={selectedProduct.title} />
+				</div>
+				<div class="form-input">
+					<label for="description">Description</label>
+					<textarea id="description" value={selectedProduct.description} />
+				</div>
+
+				<div class="form-row">
+					<div class="form-input">
+						<label for="price">Price</label>
+						<div class="price">
+							<input
+								id="price"
+								type="number"
+								min="0.01"
+								step="0.01"
+								value={selectedProduct.price}
+							/>
+						</div>
+					</div>
+
+					<div class="form-input">
+						<label for="medium">Medium</label>
+						<select id="medium" bind:value={selectedProduct.medium}>
+							<option value="Painting">Painting</option>
+							<option value="Print">Print</option>
+						</select>
+					</div>
 				</div>
 
 				<div class="form-row">
 					<div class="form-input">
 						<label for="height">Height</label>
 						<div class="inches">
-							<input name="height" type="number" min="1" step="1" value={selectedProduct.height} />
+							<input id="height" type="number" min="1" step="1" value={selectedProduct.height} />
 						</div>
 					</div>
 
 					<div class="form-input">
 						<label for="width">Width</label>
 						<div class="inches">
-							<input name="width" type="number" min="1" step="1" value={selectedProduct.width} />
+							<input id="width" type="number" min="1" step="1" value={selectedProduct.width} />
 						</div>
 					</div>
 				</div>
-
-				<label for="medium">Medium</label>
-				<select name="medium" bind:value={selectedProduct.medium}>
-					<option value="Painting">Painting</option>
-					<option value="Print">Print</option>
-				</select>
 
 				<div class="image-container">
 					{#each selectedProduct.images as image}
@@ -154,6 +178,14 @@
 		height: 34px;
 	}
 
+	textarea {
+		resize: vertical;
+	}
+
+	input {
+		width: 100%;
+	}
+
 	.hover-image {
 		position: fixed;
 		display: none;
@@ -170,12 +202,14 @@
 	.edit {
 		display: flex;
 		flex-direction: column;
+		row-gap: 1rem;
 	}
 
 	.image-container {
 		overflow-x: auto;
 		display: flex;
 		flex-direction: row;
+		border: 2px #ccc dashed;
 	}
 
 	.image-wrapper {
@@ -205,11 +239,15 @@
 	.form-row {
 		display: flex;
 		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		column-gap: 1rem;
 	}
 
 	.form-input {
 		display: flex;
 		flex-direction: column;
+		flex: 1;
 	}
 
 	.price {
@@ -228,6 +266,7 @@
 		top: 50%;
 		transform: translateY(-50%);
 		font-size: 1em;
+		font-weight: bold;
 		color: #333; /* Adjust color as needed */
 	}
 
@@ -246,7 +285,17 @@
 		right: 0.5em; /* Adjust positioning as needed */
 		top: 50%;
 		transform: translateY(-50%);
+		line-height: normal;
 		font-size: 1em;
+		font-weight: bold;
 		color: #333; /* Adjust color as needed */
+	}
+
+	.edit-header {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		line-height: normal;
 	}
 </style>
