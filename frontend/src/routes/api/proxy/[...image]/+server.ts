31c5dc293abc7c3ad5eeb7a816b9bca9d8ac4d69
Jeffrey Nicholas <jefnic23@gmail.com>
2024-11-09 00:48:48 -0500

    ✨ (proxy server): introduce new API route to fetch images from S3 bucket 💡 (proxy server): add error handling for image not found scenario to improve user experience

---
+++ frontend/src/routes/api/proxy/[...image]/+server.ts
@@ -0,0 +1,21 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { PRIVATE_BUCKETEER_BUCKET_NAME } from '$env/static/private';
+
+export const GET: RequestHandler = async ({ params }) => {
+    const imageUrl = `https://${PRIVATE_BUCKETEER_BUCKET_NAME}.s3.amazonaws.com/public/${params.image}`;
+
+    const response = await fetch(imageUrl);
+
+    if (!response.ok) {
+        return new Response('Image not found', { status: 404 });
+    }
+
+    const blob = await response.blob();
+    
+    return new Response(blob, {
+        headers: {
+            'Content-Type': blob.type,
+            'Access-Control-Allow-Origin': '*'
+        }
+    });
+};
