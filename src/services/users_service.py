b54106ee8f53b3e92df29e5e1579a4aed9ee15bf
Jeffrey Nicholas <jefnic23@gmail.com>
2024-12-08 20:38:53 -0500

    ✨ (users_service.py): introduce get_subdomain_from_user method to retrieve the storefront name associated with a user by their ID for improved user data management and functionality.

--- src/services/users_service.py
+++ src/services/users_service.py
@@ -1,6 +1,7 @@
 from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
+from src.models.storefront import Storefront
 from src.models.user import User
 
 
@@ -15,5 +16,14 @@ class UsersService:
 
     async def get_user_by_email(self, email: str) -> User | None:
         statement = select(User).where(User.email == email)
+        result = await self.session.exec(statement=statement)
+        return result.one_or_none()
+
+    async def get_subdomain_from_user(self, id: int) -> str | None:
+        statement = (
+            select(Storefront.name)
+            .join(User, User.storefront_id == Storefront.id)
+            .where(User.id == id)
+        )
         result = await self.session.exec(statement=statement)
         return result.one_or_none()
